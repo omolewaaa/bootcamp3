79899b7825f03aa3a6f2dc30b2c11b52c09121ac
omolewa <akinyomiomolewa30gmail.com>
2017-06-08 14:42:45 +0100

    Added files

---
+++ splosion/splosion.js
@@ -0,0 +1,17 @@
+/*class StringSplosion{
+	constructor(string){
+		this.string = string;
+	}
+	manipulate(){
+		let sploded = "";
+		let len = this.string.length;
+		for (let i =0; i< len; i++){
+			for (let j = 0; j <= 1; j++){
+				sploded +=this.string[j]
+			}
+		}
+        return sploded;
+	}
+}
+module.exports = StringSplosion;
+*/
